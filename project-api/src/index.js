8e7ddccf8317c8170c27e4a67ac04733a32d8ca9
Matt <faymatt96@gmail.com>
2017-11-05 01:21:10 -0400

    Add DB and Server code

--- project-api/src/index.js
+++ project-api/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path'; 
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import auth from './routes/auth';
 
 const app = express();
+app.use(bodyParser.json());
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: 'Invalid credentials '} });
-})
+mongoose.connect('mongodb://localhost/project', { useMongoClient: true });
+
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
