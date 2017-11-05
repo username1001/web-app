8ee54fe4555beee4b38ce272428ae4f0f067a572
Matt <faymatt96@gmail.com>
2017-11-04 21:15:45 -0400

    Add redux

--- project/src/index.js
+++ project/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
