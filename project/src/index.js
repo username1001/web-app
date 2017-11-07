7c6776d7eae4aedbdade7cc3c661d4e9905a38c3
Matt <faymatt96@gmail.com>
2017-11-07 03:00:26 -0500

    Update project

--- project/src/index.js
+++ project/src/index.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import decode from 'jwt-decode';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './rootReducer';
-import { userLoggedIn } from './actions/auth';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import decode from "jwt-decode";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./rootReducer";
+import { userLoggedIn } from "./actions/auth";
+import setAuthorizationHeader from "./utils/setAuthorizationHeader";
 
 const store = createStore(
-  rootReducer, 
-  composeWithDevTools(applyMiddleware(thunk))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 if (localStorage.projectJWT) {
   const payload = decode(localStorage.projectJWT);
-  const user = { 
-    token: localStorage.projectJWT, 
+  const user = {
+    token: localStorage.projectJWT,
     email: payload.email,
-    confirmed: payload.confirmed 
-  };  
+    confirmed: payload.confirmed,
+  };
+  setAuthorizationHeader(localStorage.projectJWT);
   store.dispatch(userLoggedIn(user));
 }
 
@@ -33,7 +35,6 @@ ReactDOM.render(
       <Route component={App} />
     </Provider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 registerServiceWorker();
-
