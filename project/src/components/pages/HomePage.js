3b2eaa3e062d924be39859a2deb37ee50340d4e9
Matt <faymatt96@gmail.com>
2017-11-15 07:56:10 -0500

    Add dashboard button

--- project/src/components/pages/HomePage.js
+++ project/src/components/pages/HomePage.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../../actions/auth'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../actions/auth";
 
 const HomePage = ({ isAuthenticated, logout }) => (
   <div>
     <h1>Home Page</h1>
+    <button>
+      <Link to="/dashboard">Dashboard</Link>
+    </button>
     {isAuthenticated ? (
-      <button onClick={() => logout()}>Logout</button> 
+      <button onClick={() => logout()}>Logout</button>
     ) : (
-      <div><Link to="/login">Login</Link> or <Link to='/signup'>Sign up</Link></div>
+      <div>
+        <Link to="/login">Login</Link> or <Link to="/signup">Sign up</Link>
+      </div>
     )}
   </div>
 );
 
 HomePage.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
-  logout: PropTypes.func.isRequired
+  logout: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
-    isAuthenticated: !!state.user.token
-  }
+    isAuthenticated: !!state.user.token,
+  };
 }
 
 export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
